212176b0932cc710e68a06690fabfb356d6bbe43
limaCodeLab <alan.byte.prog@outlook.com>
2024-05-30 11:52:13 -0300

    Excluded imports

--- java/spring/study/src/main/java/com/testando/study/controllers/UserController.java
+++ java/spring/study/src/main/java/com/testando/study/controllers/UserController.java
@@ -4,11 +4,13 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +18,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.testando.study.entities.User;
 import com.testando.study.services.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
--- java/spring/study/src/main/java/com/testando/study/services/UserService.java
+++ java/spring/study/src/main/java/com/testando/study/services/UserService.java
@@ -1,20 +1,16 @@
 package com.testando.study.services;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.testando.study.entities.User;
 import com.testando.study.repositories.UserRepository;
 import com.testando.study.services.exceptions.DatabaseException;
 import com.testando.study.services.exceptions.ResourceNotFoundException;
-
-import jakarta.persistence.EntityNotFoundException;
 
 @Service // Componente de Servico
 public class UserService {
