c87d2db0c8e7aaa0bf8f2479c3c92556a4613d79
limaCodeLab <alan.byte.prog@outlook.com>
2024-06-03 23:22:08 -0300

    moved entities People

--- java-jpa-base-hibernate/src/main/java/com/lifelonglearning/Application.java
+++ java-jpa-base-hibernate/src/main/java/com/lifelonglearning/Application.java
@@ -1,7 +1,7 @@
 package com.lifelonglearning;
 
 
-import entities.People;
+import com.lifelonglearning.entities.People;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
--- java-jpa-base-hibernate/src/main/java/com/lifelonglearning/entities/People.java
+++ java-jpa-base-hibernate/src/main/java/com/lifelonglearning/entities/People.java
@@ -1,4 +1,4 @@
-package entities;
+package com.lifelonglearning.entities;
 
 import jakarta.persistence.*;
 
